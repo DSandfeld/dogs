fa1cad2787f32fd62cbe21a969775175b47995f2
Daniel Sandfeld Jensen <daniesj@247e.com>
2022-11-04 10:31:18 +0100

    Creating the project

---
+++ dogs/dogs/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  dogs
+//
+//  Created by Daniel Sandfeld Jensen on 04/11/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
